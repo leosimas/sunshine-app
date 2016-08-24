5ef5638fc30f526e5197a87455ec7dd8d80388eb
fotobeacon <leonardo.simas@cpmbraxis.com>
2016-08-24 15:24:53 -0300

    listview creation

--- app/src/main/java/br/com/android/estudos/sunshineapp/MainActivityFragment.java
+++ app/src/main/java/br/com/android/estudos/sunshineapp/MainActivityFragment.java
@@ -6,6 +6,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * A placeholder fragment containing a simple view.
  */
@@ -17,6 +20,20 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        String[] forecastArray = new String[]{
+                "Today - Sunny - 88 / 63",
+                "Tomorrow - Sunny - 88 / 63",
+                "After Tomorrow - Sunny - 88 / 63",
+                "After 3  - Sunny - 88 / 63",
+                "After 4 - Sunny - 88 / 63",
+                "After 5 - Sunny - 88 / 63",
+                "Last one - Sunny - 88 / 63"
+        };
+
+        List<String> forecastList = Arrays.asList( forecastArray );
+
+
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 }
