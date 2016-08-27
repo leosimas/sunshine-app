536f48fd5f17b9f3559554db5c055521aa2a0a34
leosimas <leosimasgoncalves@gmail.com>
2016-08-27 20:10:51 -0300

    contentprovider.query

--- app/src/main/java/br/com/android/estudos/sunshineapp/data/WeatherProvider.java
+++ app/src/main/java/br/com/android/estudos/sunshineapp/data/WeatherProvider.java
@@ -190,12 +190,26 @@ public class WeatherProvider extends ContentProvider {
             }
             // "weather"
             case WEATHER: {
-                retCursor = null;
+                retCursor = mOpenHelper.getReadableDatabase().query(
+                        WeatherEntry.TABLE_NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder);
                 break;
             }
             // "location"
             case LOCATION: {
-                retCursor = null;
+                retCursor = mOpenHelper.getReadableDatabase().query(
+                        WeatherContract.LocationEntry.TABLE_NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder);
                 break;
             }
 
